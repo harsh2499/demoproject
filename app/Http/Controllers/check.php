18ac9286eab673593ddadfb99c751a35526dad33
harsh2499 <harsh.grown@gmail.com>
2023-02-13 21:17:46 +0530

    immediate

---
+++ app/Http/Controllers/check.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class check extends Controller
+{
+    public function demo()
+    {
+        echo "hello world";
+    }
+}
---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,6 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('tag') }}"><i class="nav-icon la la-question"></i> Tags</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('tagss') }}"><i class="nav-icon la la-question"></i> Tagsses</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('employee') }}"><i class="nav-icon la la-question"></i> Employees</a></li>
